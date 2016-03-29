982ec3baef144f0f1ee578bfe887931a627dae38
Dave Goehrig <dave@dloh.org>
2016-03-29 10:45:48 -0400

    Adding files

---
+++ Assets/download.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public class Barf {
+	public var foo : int;
+}
+
+function Start () {
+	
+	var 
+
+
+	var json : String = "{ \"foo\": 123 }";
+	var obj = JsonUtility.FromJson.<Barf>(json);
+	Debug.Log(obj.foo + 123 );
+}
+
+function Update () {
+
+}
